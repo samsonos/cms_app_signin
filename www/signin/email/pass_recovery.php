fb47b814d3bef370775fa229f7288f7d73bfc003
Olexandr Nazarenko <nazarenko@samsonos.com>
2014-11-10 18:58:35 +0200

    Sign in

---
+++ www/signin/email/pass_recovery.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nazarenko
+ * Date: 20.10.2014
+ * Time: 13:56
+ */
+?>
+Чтобы восстановить пароль, перейдите по <a href="<?php url_base('signin/confirm','code')?>">ссылке</a>.
